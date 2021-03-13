2d45d6d6fab119db0aa68ba8e57b332b8283aa47
Vitor Tadeu <vitor.p_souza@hotmail.com>
2021-03-13 12:16:35 -0300

    Pontos de coleta

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <v-app>
+    <v-main>
+    <PontosColeta />
+    
     <router-view/>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 
+import PontosColeta from './views/PontosColeta'
+
 export default {
   name: 'App',
-
+  components:{
+    PontosColeta
+  },
   data: () => ({
     //
   }),
