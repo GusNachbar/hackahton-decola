7c518363ac4f92ebb3048bdfcdc2a57cbaa7712d
Joyce Alberto <joyce_alberto@hotmail.com>
2021-03-13 14:14:57 -0300

    tela inicial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
     <AppTopBar/>
+    <Inicio/>
   </v-app>
 </template>
 
 
 <script>
 import AppTopBar from './components/shared/AppTopBar';
+import Inicio from './components/Inicio'
 
 export default {
   name: "App",
   components: {
     AppTopBar,
+    Inicio
   },
   data: () => ({
     //
--- src/components/shared/AppTopBar.vue
+++ src/components/shared/AppTopBar.vue
@@ -4,42 +4,42 @@
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <v-toolbar-title>Vamos nos conscientizar?</v-toolbar-title>
     </v-app-bar>
-    
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--lighten-2"
-        >
-          <router-link to="/">
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>HOME</v-list-item-title>
-            </v-list-item>
-          </router-link>
 
-          <router-link to="/">
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-delete-empty</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>PONTOS DE COLETA</v-list-item-title>
-            </v-list-item>
-          </router-link>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="green--text text--lighten-2"
+          >
+            <router-link to="/">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>HOME</v-list-item-title>
+              </v-list-item>
+            </router-link>
 
-          <router-link to="/">
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-crosshairs-question</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>SAIBA MAIS</v-list-item-title>
-            </v-list-item>
-          </router-link>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+            <router-link to="/">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-recycle</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>PONTOS DE COLETA</v-list-item-title>
+              </v-list-item>
+            </router-link>
+
+            <router-link to="/">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-crosshairs-question</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>SAIBA MAIS</v-list-item-title>
+              </v-list-item>
+            </router-link>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
   </div>
 </template>
     
@@ -54,9 +54,9 @@ export default {
 </script>
 
 <style scoped>
-  a {
-    text-decoration: none;
-  }
+a {
+  text-decoration: none;
+}
 </style>
 
 
