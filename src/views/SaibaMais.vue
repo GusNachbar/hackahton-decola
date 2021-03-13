89190e77da48c32bee68ff4b79fea902fed6d29e
xduduaraujo <xduduaraujo@gmail.com>
2021-03-13 16:23:39 -0300

    Finalização do CSS do SaibaMais

--- src/views/SaibaMais.vue
+++ src/views/SaibaMais.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-main>
 
+  <v-main class="green lighten-5">
     <v-container> 
       <v-img src="../assets/Lixos.png" alt="Lixos" class="bannerLixos d-flex justify-center mx-auto"></v-img>
     </v-container>
 
 
-    <v-list>
+    <v-list class="green lighten-5">
         <v-list-item>
           <v-list-item-icon>
               <v-img src="../assets/lixo-organico-azul.png" class="imgLixo"></v-img>
@@ -14,12 +14,12 @@
 
           <v-list-item-content>
             <v-list-item-title>Papel</v-list-item-title>
-            <v-list-item-action-text>Exemplos: jornais, revistas, impressos em geral; caixas de papelão e embalagens longa-vida.</v-list-item-action-text>
+            <v-list-item-action-text>Exemplos: jornais, revistas, impressos em geral, caixas de papelão e embalagens longa-vida.</v-list-item-action-text>
           </v-list-item-content>
         </v-list-item>
     </v-list>
 
-    <v-list>
+    <v-list class="green lighten-5">
         <v-list-item>
           <v-list-item-icon>
             <v-img src="../assets/lixo-organico-amarelo.png" class="imgLixo"></v-img>
@@ -27,12 +27,12 @@
 
           <v-list-item-content>
             <v-list-item-title>Metal</v-list-item-title>
-            <v-list-item-action-text>Exemplos: latinhas de cerveja, refrigerante e sucos; esquadrias e molduras de quadros.</v-list-item-action-text>
+            <v-list-item-action-text>Exemplos: latinhas de cerveja, refrigerante e sucos, esquadrias e molduras de quadros.</v-list-item-action-text>
           </v-list-item-content>
         </v-list-item>
     </v-list>
 
-    <v-list>
+    <v-list class="green lighten-5">
         <v-list-item>
           <v-list-item-icon>
             <v-img src="../assets/lixo-organico-marrom.png" class="imgLixo"></v-img>
@@ -40,12 +40,12 @@
 
           <v-list-item-content>
             <v-list-item-title>Orgânico</v-list-item-title>
-            <v-list-item-subtitle>Exemplos: asdasdas</v-list-item-subtitle>
+            <v-list-item-action-text>Exemplos: flores, casca de banana, sanduíches, refrigerante, leite, manteiga, couro, cabelo, geleia, gordura, salada, shampoo, tecido, papel, fruta, açúcar, animais, insetos, mel, sementes.</v-list-item-action-text>
           </v-list-item-content>
         </v-list-item>
     </v-list>    
 
-    <v-list>
+    <v-list class="green lighten-5">
         <v-list-item>
           <v-list-item-icon>
             <v-img src="../assets/lixo-organico-verde.png" class="imgLixo"></v-img>
@@ -53,12 +53,12 @@
 
           <v-list-item-content>
             <v-list-item-title>Vidro</v-list-item-title>
-            <v-list-item-subtitle>Exemplos: rascos, garrafas; vidros de conserva.</v-list-item-subtitle>
+            <v-list-item-action-text>Exemplos: rascos, garrafas, vidros de conserva.</v-list-item-action-text>
           </v-list-item-content>
         </v-list-item>
     </v-list>
 
-      <v-list>
+      <v-list class="green lighten-5">
         <v-list-item>
           <v-list-item-icon>
            <v-img src="../assets/lixo-organico-vermelho.png" class="imgLixo"></v-img>
@@ -66,11 +66,12 @@
 
           <v-list-item-content>
             <v-list-item-title>Plástico</v-list-item-title>
-            <v-list-item-subtitle>Exemplos: garrafas, embalagens de produtos de limpeza; potes de cremes e xampus; tubos e canos; brinquedos; sacos, sacolas e saquinhos de leite; papéis plastificados, metalizados ou parafinados, como embalagens de biscoito.</v-list-item-subtitle>
+            <v-list-item-action-text>Exemplos: garrafas, embalagens de produtos de limpeza, potes de cremes e xampus, tubos e canos, brinquedos, sacos, sacolas e saquinhos de leite, papéis plastificados, metalizados ou parafinados, como embalagens de biscoito.</v-list-item-action-text>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    
+   
+ 
 
   </v-main>
   
@@ -89,9 +90,13 @@ export default {};
 
 .bannerLixos {
   max-width: 500px;
-  border: 3px solid black;
-  border-radius: 7px;
+  border: 1px solid #2e7d32;
+  border-radius: 10px;
 }
 
+.v-list-item .v-list-item__title {
+  color: #1b5e20;
+  font-weight: bold;
+}
 
 </style>
