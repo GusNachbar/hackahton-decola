8bc4697ac4e051a2f5fe8f2c960f6710fd70b898
Vitor Tadeu <vitor.p_souza@hotmail.com>
2021-03-13 16:12:34 -0300

    Atualizações Pontos Coleta

--- src/views/PontosColeta.vue
+++ src/views/PontosColeta.vue
@@ -1,56 +1,52 @@
 <template>
-    <div class="purple darken-2 text-center">
-    <span class="white--text">Lorem ipsum</span>
+  <div class="green lighten-5">
     <v-container>
-         <h1>Pontos de Coleta</h1>
-         <table v-for="pontos of PontosColeta" :key="pontos.id">
-            <thead >
-                <th>  
-                    {{pontos.nome}}
-                </th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Materiais que Coleta: </td>
-                    <td v-for="categoria in pontos.categorias" :key="categoria" >
-                        {{categoria}}
-                    </td>
-                </tr>
-                <tr>
-                    <td>Endereço</td>
-                    <td class="text-right">{{ pontos.endereco }}</td>
-                </tr>
-                <td>
-                    <v-avatar size="24">
-                    <img :src="pontos.imagem"/>
-                    </v-avatar>
-                    <span class="pl-2">{{ pontos.categorias}}</span>
-                </td>
-                
-                
-            </tbody>
-        </table>
+      <h1>Pontos de Coleta</h1>
+      <table v-for="pontos of PontosColeta" :key="pontos.id">
+        <thead>
+          <th>
+            {{ pontos.nome }}
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Materiais que Coleta:</td>
+            <td v-for="categoria in pontos.categorias" :key="categoria">
+              {{ categoria }}
+            </td>
+          </tr>
+          <tr>
+            <td>Endereço</td>
+            <td class="text-right">{{ pontos.endereco }}</td>
+          </tr>
+          <td>
+            <v-avatar size="24">
+              <img :src="pontos.imagem" />
+            </v-avatar>
+            <span class="pl-2">{{ pontos.categorias }}</span>
+          </td>
+        </tbody>
+      </table>
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'PontosColeta',
-    data(){
-        return{
-            PontosColeta:[]
-        }
-    },
-    created(){
-        fetch('https://it3kjy-default-rtdb.firebaseio.com/coletaSeletiva.json').then(resposta=>resposta.json())
-        .then(json=>{
-            this.PontosColeta = json;
-        })
-    }
-}
+  name: "PontosColeta",
+  data() {
+    return {
+      PontosColeta: [],
+    };
+  },
+  created() {
+    fetch("https://it3kjy-default-rtdb.firebaseio.com/coletaSeletiva.json")
+      .then((resposta) => resposta.json())
+      .then((json) => {
+        this.PontosColeta = json;
+      });
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
